edd4f68accafe5946bc3fe2af42d78c1d17497d7
shimat <shima@sansan.com>
2020-12-19 22:47:50 +0900

    remove my_decoder.py

--- population.py
+++ population.py
@@ -2,7 +2,28 @@ import json
 import streamlit as st
 import pandas as pd
 import requests
-from my_decoder import MyDecoder
+import json
+
+class MyDecoder(json.JSONDecoder):
+    def decode(self, s):
+        result = super().decode(s)
+        return self._decode(result)
+
+    def _decode(self, o):
+        if isinstance(o, str):            
+            try:
+                if '.' in o:
+                    return float(o)
+                return int(o)
+            except ValueError:
+                return o
+        elif isinstance(o, dict):
+            return {k: self._decode(v) for k, v in o.items()}
+        elif isinstance(o, list):
+            return [self._decode(v) for v in o]
+        else:
+            return o
+
 
 @st.cache
 def load_data():
